40681de86c486e67a290156eb178130d93048f85
Ilya Stukovin <ilya.stukovin@mail.ru>
2025-04-06 22:50:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import random
 from datetime import datetime
-from flask import Flask, render_template, redirect, url_for, flash, request, jsonify, session
+from flask import Flask, render_template, redirect, flash, request, jsonify, session
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField
@@ -78,7 +78,7 @@ with app.app_context():
 @app.route('/')
 def home():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect('index')
     return render_template('login.html')
 
 
@@ -96,7 +96,7 @@ def login():
         if user and user.check_password(form.password.data):  # Проверяем пароль
             login_user(user)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('index'))
+            return redirect(next_page) if next_page else redirect('index')
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', form=form)
@@ -115,7 +115,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You can now log in', 'success')
-        return redirect(url_for('login'))
+        return redirect('login')
     return render_template('register.html', form=form)
 
 
